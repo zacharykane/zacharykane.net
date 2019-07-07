ef257849a5d551d88f19920b4c06386b28b27bbd
Zachary Kane Pride <zachary.kane@icloud.com>
2019-07-06 23:40:44 -0700

    setup service worker

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-const currentYear = document.querySelector('.currentYear');
-currentYear.textContent = new Date().getFullYear();
+(function main() {
+    const currentYear = document.querySelector('.currentYear');
+    currentYear.textContent = new Date().getFullYear();
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./service-worker.js');
+    } else {
+        console.log('Service Worker is not supported in your browser');
+    }
+})();
